5578a5b6d9f58daf4b6a69ed8e7be3a606b70cda
Rebecca Arantes <arantes.rbc@gmail.com>
2020-06-12 22:10:32 -0300

    adding socket to server and client, listening to connection and disconnection events

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
-var io = require("socket.io")(http);
 var path = require("path");
+var io = require("socket.io")(http);
 
 //route handler
 app.use(express.static(path.join(__dirname, "./client")));
+
+//listen for incoming sockets
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  //disconnection event fired by each socket
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 //http server is listening on port 3000
 http.listen(3000, () => {
